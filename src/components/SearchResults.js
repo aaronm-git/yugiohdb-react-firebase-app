0fd9321d5445a0a699f88b426f102a2a406288f2
Aaron Molina <2aaronmolina@gmail.com>
2020-04-30 14:08:07 -0400

    filtering cards by monster and spell/trap

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,7 +1,7 @@
 import React from "react";
 import firebase from "../firebase";
 import { Link } from "react-router-dom";
-import { Row, Col, Table, Spinner } from "react-bootstrap";
+import { Row, Col, Table, Spinner, Image } from "react-bootstrap";
 
 export default function SearchResults({ location }) {
   const [results, setResults] = React.useState({});
@@ -20,38 +20,71 @@ export default function SearchResults({ location }) {
 
   const OutputResults = ({ cards }) => {
     let renderThis;
+    const monsterCards = Object.keys(cards).filter((key) => cards[key].type.includes("Monster"));
+    const spellTrapCards = Object.keys(cards).filter((key) => !cards[key].type.includes("Monster"));
     if (Object.keys(cards).length) {
       renderThis = (
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Name</th>
-              <th>ATK</th>
-              <th>DEF</th>
-              <th>LVL</th>
-              <th>Race</th>
-              <th>Attribute</th>
-              <th>Type</th>
-            </tr>
-          </thead>
-          <tbody>
-            {Object.keys(cards).map((key, i) => (
-              <tr key={key}>
-                <td>{i + 1}</td>
-                <td>
-                  <Link to={`/card/${key}`}>{cards[key].name}</Link>
-                </td>
-                <td>{cards[key].atk || ""}</td>
-                <td>{cards[key].def || ""}</td>
-                <td>{cards[key].level || ""}</td>
-                <td>{cards[key].race || ""}</td>
-                <td>{cards[key].attribute}</td>
-                <td>{cards[key].type}</td>
+        <>
+          <h1>Monster Cards</h1>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Image</th>
+                <th>Name</th>
+                <th>ATK</th>
+                <th>DEF</th>
+                <th>LVL</th>
+                <th>Race</th>
+                <th>Attribute</th>
+                <th>Type</th>
               </tr>
-            ))}
-          </tbody>
-        </Table>
+            </thead>
+            <tbody>
+              {monsterCards.map((key, i) => (
+                <tr key={key}>
+                  <td>{i + 1}</td>
+                  <td>
+                    <Image src={cards[key].card_images[0].image_url_small} thumbnail />
+                  </td>
+                  <td>
+                    <Link to={`/card/${key}`}>{cards[key].name}</Link>
+                  </td>
+                  <td>{cards[key].atk}</td>
+                  <td>{cards[key].def}</td>
+                  <td>{cards[key].level}</td>
+                  <td>{cards[key].race}</td>
+                  <td>{cards[key].attribute}</td>
+                  <td>{cards[key].type}</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+
+          <h1>Spell/Trap Cards</h1>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Name</th>
+                <th>Race</th>
+                <th>Type</th>
+              </tr>
+            </thead>
+            <tbody>
+              {spellTrapCards.map((key, i) => (
+                <tr key={key}>
+                  <td>{i + 1}</td>
+                  <td>
+                    <Link to={`/card/${key}`}>{cards[key].name}</Link>
+                  </td>
+                  <td>{cards[key].race}</td>
+                  <td>{cards[key].type}</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </>
       );
     } else {
       renderThis = (
