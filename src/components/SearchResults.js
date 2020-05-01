1e694371d516ea58d7958fed8885d0fcac096432
Aaron Molina <2aaronmolina@gmail.com>
2020-05-01 14:58:42 -0400

    loading component added, updated link paths

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,7 +1,8 @@
 import React from "react";
 import firebase from "../firebase";
 import { Link, useHistory } from "react-router-dom";
-import { Row, Col, Table, Spinner, Image, Button } from "react-bootstrap";
+import { Row, Col, Table, Image, Button } from "react-bootstrap";
+import Loading from "./Loading";
 
 export default function SearchResults({ location, setCard }) {
   const [results, setResults] = React.useState({});
@@ -19,117 +20,99 @@ export default function SearchResults({ location, setCard }) {
     getSearchResults();
   }, [qb, searchTerm]);
   const setSelectedCard = (selectedCard) => {
-    console.log(selectedCard, setCard);
     setCard(selectedCard);
-    history.push(`/card`);
+    history.push(`/card/${selectedCard.id}`);
   };
   const OutputResults = ({ cards }) => {
-    let renderThis;
-    if (Object.keys(cards).length) {
-      const monsterCards = Object.keys(cards).filter((key) => cards[key].type.includes("Monster"));
-      const spellTrapCards = Object.keys(cards).filter((key) => !cards[key].type.includes("Monster"));
-      renderThis = (
-        <>
-          <h5>Monster Cards</h5>
-
-          <Table striped bordered hover size="sm" responsive>
-            <thead>
-              <tr>
-                <th>#</th>
-                <th>Image</th>
-                <th>Name</th>
-                <th>ATK</th>
-                <th>DEF</th>
-                <th>LVL</th>
-                <th>Race</th>
-                <th>Attribute</th>
-                <th>Type</th>
+    const monsterCards = Object.keys(cards).filter((key) => cards[key].type.includes("Monster"));
+    const spellTrapCards = Object.keys(cards).filter((key) => !cards[key].type.includes("Monster"));
+    return (
+      <>
+        <h5>Monster Cards</h5>
+        <Table striped bordered hover size="sm" responsive>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Image</th>
+              <th>Name</th>
+              <th>ATK</th>
+              <th>DEF</th>
+              <th>LVL</th>
+              <th>Race</th>
+              <th>Attribute</th>
+              <th>Type</th>
+            </tr>
+          </thead>
+          <tbody>
+            {monsterCards.map((key, i) => (
+              <tr key={key}>
+                <td>{i + 1}</td>
+                <td>
+                  {/* <Image src={cards[key].card_images[0].image_url_small} thumbnail /> */}
+                  <Image
+                    src="https://cdn11.bigcommerce.com/s-0kvv9/images/stencil/1280x1280/products/228856/317481/dpbcen009__44302.1519956921.jpg?c=2&imbypass=on"
+                    thumbnail
+                    style={{ maxWidth: "3.5rem" }}
+                  />
+                </td>
+                <td>
+                  <Button variant="link" size="sm" onClick={() => setSelectedCard(cards[key])}>
+                    {cards[key].name}
+                  </Button>
+                </td>
+                <td>{cards[key].atk}</td>
+                <td>{cards[key].def}</td>
+                <td>{cards[key].level}</td>
+                <td>{cards[key].race}</td>
+                <td>{cards[key].attribute}</td>
+                <td>{cards[key].type}</td>
               </tr>
-            </thead>
-            <tbody>
-              {monsterCards.map((key, i) => (
-                <tr key={key}>
-                  <td>{i + 1}</td>
-                  <td>
-                    {/* <Image src={cards[key].card_images[0].image_url_small} thumbnail /> */}
-                    <Image
-                      src="https://cdn11.bigcommerce.com/s-0kvv9/images/stencil/1280x1280/products/228856/317481/dpbcen009__44302.1519956921.jpg?c=2&imbypass=on"
-                      thumbnail
-                      style={{ maxWidth: "3.5rem" }}
-                    />
-                  </td>
-                  <td>
-                    <Button variant="link" size="sm" onClick={() => setSelectedCard(cards[key])}>
-                      {cards[key].name}
-                    </Button>
-                  </td>
-                  <td>{cards[key].atk}</td>
-                  <td>{cards[key].def}</td>
-                  <td>{cards[key].level}</td>
-                  <td>{cards[key].race}</td>
-                  <td>{cards[key].attribute}</td>
-                  <td>{cards[key].type}</td>
-                </tr>
-              ))}
-            </tbody>
-          </Table>
+            ))}
+          </tbody>
+        </Table>
+        <h5>Spell/Trap Cards</h5>
+        <Table striped bordered hover size="sm" responsive>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Image</th>
+              <th>Name</th>
+              <th>Race</th>
+              <th>Type</th>
+            </tr>
+          </thead>
+          <tbody>
+            {spellTrapCards.map((key, i) => (
+              <tr key={key}>
+                <td>{i + 1}</td>
+                <td>
+                  {/* <Image src={cards[key].card_images[0].image_url_small} thumbnail /> */}
+                  <Image
+                    src="https://usercontent1.hubstatic.com/14211330.png"
+                    style={{ maxWidth: "3.5rem" }}
+                    thumbnail
+                  />
+                </td>
 
-          <h5>Spell/Trap Cards</h5>
-          <Table striped bordered hover size="sm" responsive>
-            <thead>
-              <tr>
-                <th>#</th>
-                <th>Image</th>
-                <th>Name</th>
-                <th>Race</th>
-                <th>Type</th>
+                <td>
+                  <Button variant="link" size="sm" onClick={() => setSelectedCard(cards[key])}>
+                    {cards[key].name}
+                  </Button>{" "}
+                </td>
+                <td>{cards[key].race}</td>
+                <td>{cards[key].type}</td>
               </tr>
-            </thead>
-            <tbody>
-              {spellTrapCards.map((key, i) => (
-                <tr key={key}>
-                  <td>{i + 1}</td>
-                  <td>
-                    {/* <Image src={cards[key].card_images[0].image_url_small} thumbnail /> */}
-                    <Image
-                      src="https://usercontent1.hubstatic.com/14211330.png"
-                      style={{ maxWidth: "3.5rem" }}
-                      thumbnail
-                    />
-                  </td>
-
-                  <td>
-                    <Link to={`/card/${key}`}>{cards[key].name}</Link>
-                  </td>
-                  <td>{cards[key].race}</td>
-                  <td>{cards[key].type}</td>
-                </tr>
-              ))}
-            </tbody>
-          </Table>
-        </>
-      );
-    } else {
-      renderThis = (
-        <Spinner
-          animation="border"
-          role="status"
-          variant="warning"
-          // className="position-relative"
-          // style={{ top: "50%", left: "50%" }}
-        >
-          <span className="sr-only">Loading...</span>
-        </Spinner>
-      );
-    }
-    return renderThis;
+            ))}
+          </tbody>
+        </Table>
+      </>
+    );
   };
-
   return (
     <Row className="justify-content-md-center">
       <Col className="text-center" id="searchResultContainer">
         <h1 className="text-center">Search</h1>
-        <OutputResults cards={results} />
+        {Object.keys(results).length ? <OutputResults cards={results} /> : <Loading />}
       </Col>
     </Row>
   );
