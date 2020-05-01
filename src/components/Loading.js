f748ea3048b2db1db1c739069435f2e586018912
Aaron Molina <2aaronmolina@gmail.com>
2020-05-01 16:45:20 -0400

    spacing

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -3,7 +3,7 @@ import { Spinner } from "react-bootstrap";
 
 export default () => {
   return (
-    <div className="text-center">
+    <div className="text-center my-3">
       <Spinner animation="border" role="status" variant="warning">
         <span className="sr-only">Loading...</span>
       </Spinner>
