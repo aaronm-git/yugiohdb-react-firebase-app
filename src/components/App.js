4c096141a63a413a488f450de8090d3a72979ece
Aaron Molina <2aaronmolina@gmail.com>
2020-05-01 16:13:55 -0400

    Updated to use Hash

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import { Container, Row, Col, Card } from "react-bootstrap";
 
 import SearchResults from "./SearchResults";
