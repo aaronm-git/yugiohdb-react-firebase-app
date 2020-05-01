49cc6f4114b092c94ef779847bd5d6ad1b3ef782
Aaron Molina <2aaronmolina@gmail.com>
2020-05-01 08:25:52 -0400

    remove links not being used

--- src/components/SiteNavBar.js
+++ src/components/SiteNavBar.js
@@ -13,31 +13,16 @@ export default function SiteNavBar() {
     history.push(`/search?q=${searchTerm}`);
   };
   return (
-    <Navbar bg="light" expand="lg" className="rounded shadow-lg bg-dark" variant="dark">
+    <Navbar expand="lg" className="rounded shadow-lg justify-content-between" variant="dark" bg="dark">
       <Link to="/" className="navbar-brand">
         {process.env.REACT_APP_SITE_TITLE}
       </Link>
       <Form inline className="position-relative" onSubmit={searchTermSubmit}>
         <FormControl type="text" className="mr-sm-2" placeholder="Search" onChange={updateSearchTerm} />
         <Link to={`/search/?q=${searchTerm}`} className="btn btn-outline-warning">
-          Search
+          SEARCH
         </Link>
       </Form>
-      {/* <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <li className="nav-item">
-            <Link to="/" className="nav-link">
-              Home
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/card" className="nav-link">
-              Card
-            </Link>
-          </li>
-        </Nav>
-      </Navbar.Collapse> */}
     </Navbar>
   );
 }
