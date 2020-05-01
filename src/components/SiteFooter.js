79e26249192fb1ddd17fb5fa0f6c618f6a0f3264
Aaron Molina <2aaronmolina@gmail.com>
2020-04-30 22:31:04 -0400

    environment added

--- src/components/SiteFooter.js
+++ src/components/SiteFooter.js
@@ -2,100 +2,16 @@ import React from "react";
 import { Navbar } from "react-bootstrap";
 
 export default function SiteFooter() {
+  const Environment = () => {
+    if (process.env.NODE_ENV === "development") return <h5 className="text-uppercase">{process.env.NODE_ENV}</h5>;
+    else return null;
+  };
   return (
     <Navbar className="pt-4">
       <div className="row">
-        <div className="col-12 col-md">
-          {/* <img className="mb-2" src="" alt="" width="24" height="24"></img> */}
-          <small className="d-block mb-3 text-light">© 2020</small>
-        </div>
-        <div className="col-6 col-md">
-          <h5 className="text-secondary">Features</h5>
-          {/* <ul className="list-unstyled text-small">
-            <li>
-              <a className="text-light" href="#">
-                Cool stuff
-              </a>
-            </li>
-            <li>
-              <a className="text-light" href="#">
-                Random feature
-              </a>
-            </li>
-            <li>
-              <a className="text-light" href="#">
-                Team feature
-              </a>
-            </li>
-            <li>
-              <a className="text-light" href="#">
-                Stuff for developers
-              </a>
-            </li>
-            <li>
-              <a className="text-light" href="#">
-                Another one
-              </a>
-            </li>
-            <li>
-              <a className="text-light" href="#">
-                Last time
-              </a>
-            </li>
-          </ul>
-        */}
-        </div>
-        <div className="col-6 col-md">
-          <h5 className="text-secondary">Resources</h5>
-          {/* <ul className="list-unstyled text-small">
-            <li>
-              <a className="text-light" href="#">
-                Resource
-              </a>
-            </li>
-            <li>
-              <a className="text-light" href="#">
-                Resource name
-              </a>
-            </li>
-            <li>
-              <a className="text-light" href="#">
-                Another resource
-              </a>
-            </li>
-            <li>
-              <a className="text-light" href="#">
-                Final resource
-              </a>
-            </li>
-          </ul>
-         */}
-        </div>
-        <div className="col-6 col-md">
-          <h5 className="text-secondary">About</h5>
-          {/* <ul className="list-unstyled text-small">
-            <li>
-              <a className="text-light" href="#">
-                Team
-              </a>
-            </li>
-            <li>
-              <a className="text-light" href="#">
-                Locations
-              </a>
-            </li>
-            <li>
-              <a className="text-light" href="#">
-                Privacy
-              </a>
-            </li>
-            <li>
-              <a className="text-light" href="#">
-                Terms
-              </a>
-            </li>
-          </ul>
-        */}
+        <div className="col-12 col-md text-light">
+          <small className="d-block mb-3">© 2020</small>
+          <Environment />
         </div>
       </div>
     </Navbar>
