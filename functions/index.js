2dff3b19173c49740417c57d22fd9f062a2b7e3c
Aaron Molina <2aaronmolina@gmail.com>
2020-05-04 20:31:07 -0400

    added cloud function to make a thumbnail size img, in my storage bucket when a new image is uploaded

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,13 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const fetch = require("node-fetch");
-
+const spawn = require("child-process-promise").spawn;
+const path = require("path");
+const os = require("os");
+const fs = require("fs");
 admin.initializeApp();
 const storage = admin.storage();
 const cors = require("cors")({ origin: true });
-
 class apiResponse {
   constructor(Success = false, Status = 200, Data = {}, Message = null) {
     this.Success = Success;
@@ -53,6 +55,47 @@ exports.verifyImgExists = functions.https.onRequest((req, res) => {
     }
     return res.status(httpResponse.Status).send({ data: httpResponse });
   });
+});
+
+exports.generateThumbnail = functions.storage.object().onFinalize(async (object) => {
+  const filePath = object.name; // File path in the bucket.
+  const contentType = object.contentType; // File content type.
+  const metageneration = object.metageneration; // Number of times metadata has been generated. New objects have a value of 1.
+
+  // Exit if this is triggered on a file that is not an image.
+  if (!contentType.startsWith("image/")) {
+    return console.log("This is not an image.");
+  }
+
+  // Get the file name.
+  const fileName = path.basename(filePath);
+  // Exit if the image is already a thumbnail.
+  if (path.dirname(filePath) === "card_thumbnails") {
+    return console.log("Already a Thumbnail.");
+  }
+
+  // Download file from bucket.
+  const bucket = storage.bucket();
+  const tempFilePath = path.join(os.tmpdir(), fileName);
+  const metadata = {
+    contentType: contentType,
+  };
+  await bucket.file(filePath).download({
+    destination: tempFilePath,
+  });
+  console.log("Image downloaded locally to", tempFilePath);
+  // Generate a thumbnail using ImageMagick.
+  await spawn("convert", [tempFilePath, "-thumbnail", "200x200>", tempFilePath]);
+  console.log("Thumbnail created at", tempFilePath);
+  // Set new file path
+  const thumbFilePath = `card_thumbnails/${fileName}`;
+  // Uploading the thumbnail.
+  await bucket.upload(tempFilePath, {
+    destination: thumbFilePath,
+    metadata: metadata,
+  });
+  // Once the thumbnail has been uploaded delete the local file to free up disk space.
+  return fs.unlinkSync(tempFilePath);
 });
 //
 // ================================================================================
