dea9a23523c9ade2e23f8a7a72f2052f578e4833
Aaron Molina <28906210+aaronm-git@users.noreply.github.com>
2020-05-12 22:51:13 -0400

    navbar updated

--- src/components/SiteNavBar.js
+++ src/components/SiteNavBar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link, useHistory } from "react-router-dom";
-import { Navbar, Form, FormControl } from "react-bootstrap";
+import { Navbar, Form, FormControl, Image } from "react-bootstrap";
+import logo from "../yugioh-logo.png"
 
 export default function SiteNavBar() {
   const history = useHistory();
@@ -14,11 +15,11 @@ export default function SiteNavBar() {
   };
   return (
     <Navbar expand="lg" className="rounded shadow-lg justify-content-between" variant="dark" bg="dark">
-      <Link to="/" className="navbar-brand">
-        {process.env.REACT_APP_SITE_TITLE}
+      <Link to="/" className="navbar-brand d-block mx-auto">
+        <Image src={logo} alt={process.env.REACT_APP_SITE_TITLE} fluid style={{maxHeight: "4rem"}}/>
       </Link>
-      <Form inline className="position-relative" onSubmit={searchTermSubmit}>
-        <FormControl type="text" className="mr-sm-2" placeholder="Search" onChange={updateSearchTerm} />
+      <Form onSubmit={searchTermSubmit} className="w-100 d-flex justify-content-center justify-content-lg-end align-items-center">
+        <FormControl type="text" className="w-50 mr-2" placeholder="Search" onChange={updateSearchTerm} style={{maxWidth: "200px"}}/>
         <Link to={`/search/?q=${searchTerm}`} className="btn btn-outline-warning">
           SEARCH
         </Link>
