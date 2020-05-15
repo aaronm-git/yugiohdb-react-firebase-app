f4c9411495256d062caad9c599062000e99d8729
Aaron Molina <28906210+aaronm-git@users.noreply.github.com>
2020-05-14 22:54:09 -0400

    added environment variable to algolia admin key

---
+++ functions/algolia/createIndex.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+
+const cardsDump = require("../../dump/allcards.json").data;
+const algoliaCard = require("./card");
+
+const algoliasearch = require("algoliasearch");
+const algoliaAdminKey = process.env.ALGOLIA_ADMIN_KEY;
+const algoliaAppId = "A5JPX9U9RD";
+const algoliaIndexName = "cards";
+
+const algoliaClient = algoliasearch(algoliaAppId, algoliaAdminKey);
+const algoliaIndex = algoliaClient.initIndex(algoliaIndexName);
+
+const cards = [];
+cardsDump.forEach((card) => {
+  const algoliaCardObject = new algoliaCard(card);
+  cards.push(algoliaCardObject);
+});
+console.log(cards.find((card) => card.objectID === 55794644));
+// const jsonObj = ;
+// const jsonContent = JSON.stringify(data);
+// fs.writeFile("cards.json", jsonContent, "utf8", (err) => {
+//   if (err) {
+//     console.log("An error occured while writing JSON Object to File.");
+//     return console.log(err);
+//   }
+// });
+
+algoliaIndex
+  .saveObjects(cards)
+  .then(() => console.log("complete!"))
+  .catch((error) => console.error(error));
---
+++ functions/algolia/updateIndex.js
@@ -0,0 +1,41 @@
+const cards = require("../../dump/allcards.json").data;
+
+const algoliasearch = require("algoliasearch");
+const algoliaAdminKey = process.env.ALGOLIA_ADMIN_KEY;
+const algoliaAppId = "A5JPX9U9RD";
+const algoliaIndexName = "cards";
+
+const algoliaClient = algoliasearch(algoliaAppId, algoliaAdminKey);
+const index = algoliaClient.initIndex(algoliaIndexName);
+
+class algoliaCardItem {
+  constructor(card) {
+    this.objectID = card.id;
+    this.name = card.name;
+    this.description = card.desc;
+    this.atk = card.atk;
+    this.def = card.def;
+    this._tags = [card.type, card.race, card.archetype].filter((tag) => tag !== undefined);
+    this.imageURLs = {
+      full: `https://storage.googleapis.com/yugiohdb-app.appspot.com/cards/${card.id}.jpg`,
+      thumbnail: `https://storage.googleapis.com/yugiohdb-app.appspot.com/card_thumbnails/${card.id}.jpg`,
+    };
+  }
+}
+
+const algoliaObjects = [];
+cards.forEach((card) => {
+  algoliaObject = new algoliaCardItem(card);
+  algoliaObjects.push(algoliaObject);
+});
+// const dataJSON = JSON.stringify(algoliaObjects);
+//   fs.writeFile("cards.json", jsonContent, "utf8", (err) => {
+//     if (err) {
+//       console.log("An error occured while writing JSON Object to File.");
+//       return console.log(err);
+//     }
+//   });
+index
+  .replaceAllObjects(algoliaObjects)
+  .then(() => console.log("complete!"))
+  .catch((error) => console.error(error));
