1bbc21feff8a4a53dc36536145483f51f90ec4e2
Aaron Molina <28906210+aaronm-git@users.noreply.github.com>
2020-05-22 01:48:28 -0400

    token crash fixed

--- src/components/AlgoliaSearch.js
+++ src/components/AlgoliaSearch.js
@@ -68,7 +68,7 @@ export default function AlgoliaSearch({ location, selectThisCard }) {
                 {typeof hit.atk === "number" && typeof hit.def === "number" ? (
                   <div className="d-flex justify-content-between">
                     <span>{hit.atk}/{hit.def}</span>
-                    <span>{hit.type.lvl2.substring(hit.type.lvl2.lastIndexOf(" "))}</span>
+                    <span>{hit.type.lvl2? hit.type.lvl2.substring(hit.type.lvl2.lastIndexOf(" ")): hit.type.lvl0}</span>
                   </div>
                 ) : (
                 <span>{hit.type.lvl0}/{hit.race}</span>
